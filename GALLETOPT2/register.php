64335a7c8da2068000a2e5c1fb5e8dc86652c04c
ZerodeathsYanyan <159798099+ZerodeathsYanyan@users.noreply.github.com>
2024-12-13 21:45:18 +0800

    register.php

--- GALLETOPT2/register.php
+++ GALLETOPT2/register.php
@@ -7,6 +7,7 @@ require_once 'core/handleForms.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	   <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
 	<title>Document</title>
 	<style>
 		body {
